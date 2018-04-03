7b5cc452b91fccf5609f9a7eedcdfb2189b6e097
jason <jf.zhang@bigsec.com>
2018-04-03 10:47:32 +0800

    提交

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,17 @@
 
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   parserOptions: {
-    parser: 'babel-eslint'
+    sourceType: 'module'
   },
   env: {
     browser: true,
   },
-  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-  extends: ['plugin:vue/essential', 'airbnb-base'],
+  extends: 'airbnb-base',
   // required to lint *.vue files
   plugins: [
-    'vue'
+    'html'
   ],
   // check if imports actually resolve
   settings: {
@@ -23,30 +22,23 @@ module.exports = {
       }
     }
   },
+  globals: {
+    document: true
+  },
   // add your custom rules here
   rules: {
     // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
+    'import/extensions': ['off', 'always', {
       js: 'never',
       vue: 'never'
     }],
-    // disallow reassignment of function parameters
-    // disallow parameter object manipulation except for specific exclusions
-    'no-param-reassign': ['error', {
-      props: true,
-      ignorePropertyModificationsFor: [
-        'state', // for vuex state
-        'acc', // for reduce accumulators
-        'e' // for e.returnvalue
-      ]
-    }],
+    'import/no-unresolved': [0, {commonjs: true, amd: true}],
     // allow optionalDependencies
     'import/no-extraneous-dependencies': ['error', {
       optionalDependencies: ['test/unit/index.js']
     }],
-    "comma-dangle": ["error", "never"],
-    "indent": "off",
+    'comma-dangle': ['error', 'never'],
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
-};
+}
